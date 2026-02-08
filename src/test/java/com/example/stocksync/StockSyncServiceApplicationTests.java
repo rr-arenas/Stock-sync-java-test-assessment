39a107cc1100fa62265ab110176cfb30dcfc2a94
rr-arenas <rr.arenas013@gmail.com>
2026-02-08 11:13:22 +0800

    initial java springboot project

---
+++ src/test/java/com/example/stocksync/StockSyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocksync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSyncServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
